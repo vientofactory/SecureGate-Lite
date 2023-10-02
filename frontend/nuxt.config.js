9b4519b4ff3d5ed181312402de2bf5aa06ac0d47
vientorepublic <markgod0419@gmail.com>
2023-10-03 01:56:29 +0900

    ✅ nuxt build settings

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -1,43 +1,41 @@
-import 'dotenv/config';
+import "dotenv/config";
 
 export default {
   head: {
-    titleTemplate: '%s - SecureGate Lite',
+    titleTemplate: "%s - SecureGate Lite",
     htmlAttrs: {
-      lang: 'ko'
+      lang: "ko",
     },
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { name: 'google-adsense-account', content: 'ca-pub-6469556883738104' },
-      { hid: 'description', name: 'description', content: '디스코드 링크 보호 솔루션' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { name: "google-adsense-account", content: "ca-pub-6469556883738104" },
+      { hid: "description", name: "description", content: "디스코드 링크 보호 솔루션" },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/images/favicon/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/images/favicon/favicon.ico" }],
   },
 
   proxy: {
-    '/api': {
+    "/api": {
       target: process.env.BACKEND_HOST,
-      pathRewrite: { '^/api': '' }
-    }
+      pathRewrite: { "^/api": "" },
+    },
   },
 
   axios: {
     proxy: true,
     baseURL: process.env.FRONTEND_HOST,
     proxyHeaders: false,
-    credentials: false
+    credentials: false,
   },
 
   router: {
-    fallback: true
+    fallback: true,
   },
 
   loading: {
-    color: '#29d',
-    failedColor: '#29d'
+    color: "#29d",
+    failedColor: "#29d",
   },
 
   auth: {
@@ -45,91 +43,78 @@ export default {
       discord: {
         clientId: process.env.CLIENT_ID,
         clientSecret: process.env.CLIENT_SECRET,
-        codeChallengeMethod: 'S256',
-        grantType: 'authorization_code',
-        scope: ['email', 'identify', 'guilds', 'guilds.join']
+        codeChallengeMethod: "S256",
+        grantType: "authorization_code",
+        scope: ["email", "identify", "guilds", "guilds.join"],
       },
     },
     redirect: {
-      login: '/oauth2/login',
-      logout: '/oauth2/logout',
-      callback: '/oauth2/callback',
-      home: '/'
-    }
+      login: "/oauth2/login",
+      logout: "/oauth2/logout",
+      callback: "/oauth2/callback",
+      home: "/",
+    },
   },
 
   recaptcha: {
     hideBadge: false,
     siteKey: process.env.RECAPTCHA_SITE,
     version: 2,
-    size: 'normal'
+    size: "normal",
   },
 
   dayjs: {
-    defaultTimeZone: 'Asia/Seoul',
-    plugins: [
-      'utc',
-      'timezone'
-    ]
+    defaultTimeZone: "Asia/Seoul",
+    plugins: ["utc", "timezone"],
   },
 
-  css: [
-    '~/static/css/main.css',
-    '~/static/css/vuetify.css'
-  ],
+  css: ["~/static/css/main.css", "~/static/css/vuetify.css"],
 
-  plugins: [
-  ],
+  plugins: [],
 
   components: true,
 
   buildModules: [
-    '@nuxtjs/vuetify',
+    "@nuxtjs/vuetify",
     [
-      '@nuxtjs/dotenv',
+      "@nuxtjs/dotenv",
       {
         //CLIENT SECRET등이 노출되지 않도록 스코프 지정.
-        only: ['VER', 'FRONTEND_HOST', 'CLIENT_ID', 'PERMISSIONS', 'BRAND']
-      }
-    ]
+        only: ["VER", "FRONTEND_HOST", "CLIENT_ID", "PERMISSIONS", "BRAND"],
+      },
+    ],
   ],
 
-  modules: [
-    '@nuxtjs/axios',
-    '@nuxtjs/auth-next',
-    '@nuxtjs/proxy',
-    'cookie-universal-nuxt',
-    '@nuxtjs/recaptcha',
-    '@nuxtjs/i18n',
-    '@nuxtjs/dayjs'
-  ],
+  modules: ["@nuxtjs/axios", "@nuxtjs/auth-next", "@nuxtjs/proxy", "cookie-universal-nuxt", "@nuxtjs/recaptcha", "@nuxtjs/i18n", "@nuxtjs/dayjs"],
 
   i18n: {
-    strategy: 'no_prefix',
-    locales: ['en', 'ko'],
-    defaultLocale: 'ko',
+    strategy: "no_prefix",
+    locales: ["en", "ko"],
+    defaultLocale: "ko",
     vueI18n: {
-      fallbackLocale: 'ko'
+      fallbackLocale: "ko",
     },
-    vueI18nLoader: true
+    vueI18nLoader: true,
   },
 
   vuetify: {
     theme: {
       disable: true,
-      dark: true
-    }
+      dark: true,
+    },
   },
 
   build: {
-    publicPath: '/static/',
-    extractCSS: true,
+    publicPath: "/static/",
     minimize: true,
     splitChunks: {
-      chunks: 'all',
-      automaticNameDelimiter: '.',
+      chunks: "all",
+      automaticNameDelimiter: ".",
       name: undefined,
-      cacheGroups: {}
+      cacheGroups: {},
+    },
+    extractCSS: {
+      ignoreOrder: true,
     },
     html: {
       minify: {
@@ -146,11 +131,9 @@ export default {
         removeComments: true,
         removeEmptyElements: true,
         preserveLineBreaks: false,
-        collapseWhitespace: true
-      }
+        collapseWhitespace: true,
+      },
     },
-    transpile: [
-      'defu'
-    ]
-  }
-}
+    transpile: ["defu"],
+  },
+};
